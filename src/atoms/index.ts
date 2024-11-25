ae68a4c8521913861c701f8b918650ae030396ec
markshawn2020 <shawninjuly@gmail.com>
2024-11-26 06:35:12 +0800

    refactor(atoms): Refactor atoms to use atomWithStorage for persistence
    - Updated nameAtom, birthYearAtom, genderAtom, introductionAtom, mbtiSelectionsAtom, and currentStepAtom to use atomWithStorage for data persistence.
    - Added comments to indicate purpose of each atom.
    - Removed unnecessary comments and unused imports.
    
    - Form Errors Atom doesn't need persistence and remains unchanged for temporary UI state.

--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -1,20 +1,17 @@
-// import { atomWithStorage } from "jotai/utils"
-// import { taroStorage } from "../utils/storage"
-
 import { atom } from "jotai"
 import { atomWithStorage } from "jotai/utils"
 import { taroStorage } from "../utils/storage"
 
 // Form Data Atoms
-export const nameAtom = atom("")
-export const birthYearAtom = atom("")
-export const genderAtom = atom("")
-export const introductionAtom = atom("")
+export const nameAtom = atomWithStorage("userName", "", taroStorage)
+export const birthYearAtom = atomWithStorage("userBirthYear", "", taroStorage)
+export const genderAtom = atomWithStorage("userGender", "", taroStorage)
+export const introductionAtom = atomWithStorage("userIntroduction", "", taroStorage)
 export const colorThemeAtom = atomWithStorage("colorTheme", "dark", taroStorage)
-export const mbtiSelectionsAtom = atom([false, false, false, false])
-export const currentStepAtom = atom(2)
+export const mbtiSelectionsAtom = atomWithStorage("mbtiSelections", [false, false, false, false], taroStorage)
+export const currentStepAtom = atomWithStorage("currentStep", 2, taroStorage)
 
-// Form Errors Atom
+// Form Errors Atom - This doesn't need persistence as it's temporary UI state
 export const formErrorsAtom = atom({
   name: "",
   birthYear: "",
